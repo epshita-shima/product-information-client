caf2045c78e2dd27cbb48ae6b39dda5292b11260
epshita-shima <epshitashima@gmail.com>
2022-08-23 09:19:25 +0600

    complete header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
+          primary: "#4b5320",
+          secondary: "#D3D3D3",
+          accent: "#ffa6c8",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
